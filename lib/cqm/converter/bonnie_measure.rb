e39465a340a56ea4e1d8ee4737756ae4bc6763bd
Chris Hossenlopp <hossenlopp@mitre.org>
2018-11-16 16:41:49 -0500

    actual tests and code for population conversion work

--- lib/cqm/converter/bonnie_measure.rb
+++ lib/cqm/converter/bonnie_measure.rb
@@ -14,18 +14,20 @@ module CQM::Converter
       cqm_measure.title = bonnie_measure.title
       cqm_measure.description = bonnie_measure.description
       cqm_measure.calculate_sdes = bonnie_measure.calculate_sdes
+      cqm_measure.main_cql_library = bonnie_measure.main_cql_library
 
       # more complicated fields
       cqm_measure.measure_scoring = if bonnie_measure.continuous_variable then 'CONTINUOUS_VARIABLE' else 'PROPORTION' end
       cqm_measure.calculation_method = if bonnie_measure.episode_of_care then 'EPISODE_OF_CARE' else 'PATIENT' end
 
       # cql_libraries
-      cqm_measure.cql_libraries = bonnie_measure.elm.map do |elm|
+      bonnie_measure.elm.each_with_index do |elm, elm_index|
         #require 'pry'; binding.pry
         cql_library = CQM::CQLLibrary.new(
           library_name: elm['library']['identifier']['id'],
           library_version: elm['library']['identifier']['version'],
-          elm: elm
+          elm: elm,
+          cql: bonnie_measure.cql[elm_index]
         )
         
         cql_library.elm_annotations = bonnie_measure.elm_annotations[cql_library.library_name]
@@ -33,16 +35,76 @@ module CQM::Converter
         # convert statement dependencies to new form
         bonnie_measure.cql_statement_dependencies[cql_library.library_name].each do |statement_name, dependencies|
           statement_dependency = CQM::StatementDependency.new(statement_name: statement_name)
+          # TODO: consider removing duplicates
           statement_dependency.statement_references = dependencies.map do |dependency|
             CQM::StatementReference.new(library_name: dependency['library_name'], statement_name: dependency['statement_name'])
           end
           cql_library.statement_dependencies << statement_dependency
         end
 
-        cql_library
+        cqm_measure.cql_libraries << cql_library
+      end
+
+
+      cqm_measure.population_criteria = bonnie_measure.population_criteria
+      cqm_measure.data_criteria = bonnie_measure.data_criteria
+      cqm_measure.source_data_criteria = bonnie_measure.source_data_criteria
+      cqm_measure.measure_period = bonnie_measure.measure_period
+      cqm_measure.measure_attributes = bonnie_measure.measure_attributes
+
+      bonnie_measure.populations.each_with_index do |bonnie_population, pop_index|
+        population_set = CQM::PopulationSet.new(
+          title: bonnie_population['title'],
+          id: bonnie_population['id']
+        )
+        
+        # construct the population map and fill it
+        population_map = construct_population_map(bonnie_population)
+        bonnie_population.each_pair do |population_name, population_key|
+          # make sure it isnt a metadate
+          if !['id', 'title'].include?(population_name)
+            population_map[population_name.to_sym] = CQM::StatementReference.new(
+              library_name: cqm_measure.main_cql_library,
+              statement_name: get_cql_statement_for_bonnie_population_key(bonnie_measure.populations_cql_map, population_key)
+            )
+          end
+        end
+
+        population_set.populations = population_map
+        # TODO: add SDEs, STRATS, and observations
+        cqm_measure.population_sets << population_set
       end
 
       cqm_measure
+    end
+
+    private
+    def self.construct_population_map(bonnie_population)
+      # is CV if it has MSRPOPL
+      if (bonnie_population.has_key?('MSRPOPL'))
+        return CQM::ContinuousVariablePopulationMap.new()
+      # is porportion if it has DENEXCEP
+      elsif (bonnie_population.has_key?('DENEXCEP'))
+        return CQM::PorportionPopulationMap.new()
+      # is ratio if it has NUMER
+      elsif (bonnie_population.has_key?('NUMER'))
+        return CQM::RatioPopulationMap.new()
+      # Otherwise, it is cohort
+      else
+        return CQM::CohortPopulationMap.new()
+      end
+    end
+
+    def self.get_cql_statement_for_bonnie_population_key(populations_cql_map, population_key)
+      if population_key.include?('_')
+        pop_name, pop_index = population_key.split('_')
+        pop_index = pop_index.to_i
+      else
+        pop_name = population_key
+        pop_index = 0
+      end
+
+      populations_cql_map[pop_name][pop_index]
     end
   end
 end
--- spec/cqm/converter/bonnie_measure_spec.rb
+++ spec/cqm/converter/bonnie_measure_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe CQM::Converter::BonnieMeasure do
 
   it 'converts proportion measure with single population set' do
-    bonnie_measure = CqlMeasure.new.from_json(File.read('spec/fixtures/bonnie/cms134v6.json'))
+    bonnie_measure = CqlMeasure.new.from_json(File.read('spec/fixtures/bonnie/CMS134v6.json'))
     cqm_measure = CQM::Converter::BonnieMeasure.to_cqm(bonnie_measure)
 
     expect(cqm_measure).to_not be_nil
@@ -12,15 +12,141 @@ RSpec.describe CQM::Converter::BonnieMeasure do
 
     expect(cqm_measure.cql_libraries.size).to eq(3)
 
-    require 'pry'; binding.pry
-    main_library = cqm_measure.cql_libraries.select { |lib| lib.library_name == 'DiabetesMedicalAttentionforNephropathy' }.first
+    # check the main library name and find new library structure using it
+    expect(cqm_measure.main_cql_library).to eq('DiabetesMedicalAttentionforNephropathy')
+    main_library = cqm_measure.cql_libraries.select { |lib| lib.library_name == cqm_measure.main_cql_library }.first
 
+    # check the new library structure
     expect(main_library).to_not be_nil
     expect(main_library.library_name).to eq('DiabetesMedicalAttentionforNephropathy')
     expect(main_library.library_version).to eq('6.1.003')
     expect(main_library.statement_dependencies.size).to eq(18)
-    expect(main_library)
+    expect(main_library.elm_annotations).to eq(bonnie_measure.elm_annotations['DiabetesMedicalAttentionforNephropathy'])
+    expect(main_library.elm).to eq(bonnie_measure.elm[0])
+    expect(main_library.cql).to start_with('library DiabetesMedicalAttentionforNephropathy')
 
+    # check the references used by the "Initial Population"
+    ipp_dep = main_library.statement_dependencies.select { |dep| dep.statement_name == "Initial Population"}.first
+    expect(ipp_dep).to_not be_nil
+    expect(ipp_dep.statement_references.size).to eq(3)
+    expect(ipp_dep.statement_references.map(&:statement_name)).to include("Qualifying Encounters")
+
+    # Legacy fields that may be removed later
+    expect(cqm_measure.population_criteria).to eq(bonnie_measure.population_criteria)
+    expect(cqm_measure.data_criteria).to eq(bonnie_measure.data_criteria)
+    expect(cqm_measure.source_data_criteria).to eq(bonnie_measure.source_data_criteria)
+    expect(cqm_measure.measure_period).to eq(bonnie_measure.measure_period)
+    expect(cqm_measure.measure_attributes).to eq(bonnie_measure.measure_attributes)
+
+    # check population set
+    expect(cqm_measure.population_sets.size).to eq(1)
+    population_set = cqm_measure.population_sets[0]
+    expect(population_set.id).to eq('PopulationCriteria1')
+    expect(population_set.title).to eq('Population Criteria Section')
+    expect(population_set.populations).to be_instance_of(CQM::RatioPopulationMap)
+    expect(population_set.populations.IPP.statement_name).to eq("Initial Population")
   end
 
+  it 'converts proportion measure with three population sets' do
+    bonnie_measure = CqlMeasure.new.from_json(File.read('spec/fixtures/bonnie/CMS160v6.json'))
+    cqm_measure = CQM::Converter::BonnieMeasure.to_cqm(bonnie_measure)
+
+    expect(cqm_measure).to_not be_nil
+    expect(cqm_measure.measure_scoring).to eq('PROPORTION')
+    expect(cqm_measure.calculation_method).to eq('PATIENT')
+
+    expect(cqm_measure.cql_libraries.size).to eq(1)
+
+    # check the main library name and find new library structure using it
+    expect(cqm_measure.main_cql_library).to eq('DepressionUtilizationofthePHQ9Tool')
+    main_library = cqm_measure.cql_libraries.select { |lib| lib.library_name == cqm_measure.main_cql_library }.first
+
+    # check the new library structure
+    expect(main_library).to_not be_nil
+    expect(main_library.library_name).to eq('DepressionUtilizationofthePHQ9Tool')
+    expect(main_library.library_version).to eq('6.1.001')
+    expect(main_library.statement_dependencies.size).to eq(33)
+    expect(main_library.elm_annotations).to eq(bonnie_measure.elm_annotations['DepressionUtilizationofthePHQ9Tool'])
+    expect(main_library.elm).to eq(bonnie_measure.elm[0])
+    expect(main_library.cql).to start_with('library DepressionUtilizationofthePHQ9Tool')
+
+    # check the references used by the "Initial Population 1"
+    ipp_dep = main_library.statement_dependencies.select { |dep| dep.statement_name == "Initial Population 1"}.first
+    expect(ipp_dep).to_not be_nil
+    expect(ipp_dep.statement_references.size).to eq(3)
+    expect(ipp_dep.statement_references.map(&:statement_name)).to include("Depression Face to Face Encounter 1")
+
+    # Legacy fields that may be removed later
+    expect(cqm_measure.population_criteria).to eq(bonnie_measure.population_criteria)
+    expect(cqm_measure.data_criteria).to eq(bonnie_measure.data_criteria)
+    expect(cqm_measure.source_data_criteria).to eq(bonnie_measure.source_data_criteria)
+    expect(cqm_measure.measure_period).to eq(bonnie_measure.measure_period)
+    expect(cqm_measure.measure_attributes).to eq(bonnie_measure.measure_attributes)
+
+    # check population set 1
+    expect(cqm_measure.population_sets.size).to eq(3)
+    population_set = cqm_measure.population_sets[0]
+    expect(population_set.id).to eq('PopulationCriteria1')
+    expect(population_set.title).to eq('Population Criteria Section 1')
+    expect(population_set.populations).to be_instance_of(CQM::RatioPopulationMap)
+    expect(population_set.populations.IPP.statement_name).to eq("Initial Population 1")
+
+    population_set = cqm_measure.population_sets[1]
+    expect(population_set.id).to eq('PopulationCriteria2')
+    expect(population_set.title).to eq('Population Criteria Section 2')
+    expect(population_set.populations).to be_instance_of(CQM::RatioPopulationMap)
+    expect(population_set.populations.DENOM.statement_name).to eq("Denominator 2")
+
+    population_set = cqm_measure.population_sets[2]
+    expect(population_set.id).to eq('PopulationCriteria3')
+    expect(population_set.title).to eq('Population Criteria Section 3')
+    expect(population_set.populations).to be_instance_of(CQM::RatioPopulationMap)
+    expect(population_set.populations.NUMER.statement_name).to eq("Numerator 3")
+  end
+
+
+  it 'converts continuous variable episode measure with single population set and three stratifications' do
+    bonnie_measure = CqlMeasure.new.from_json(File.read('spec/fixtures/bonnie/CMS32v7.json'))
+    cqm_measure = CQM::Converter::BonnieMeasure.to_cqm(bonnie_measure)
+
+    expect(cqm_measure).to_not be_nil
+    expect(cqm_measure.measure_scoring).to eq('CONTNIUOUS_VARIABLE')
+    expect(cqm_measure.calculation_method).to eq('EPISODE')
+
+    expect(cqm_measure.cql_libraries.size).to eq(1)
+
+    # check the main library name and find new library structure using it
+    expect(cqm_measure.main_cql_library).to eq('MedianTimefromEDArrivaltoEDDepartureforDischargedEDPatients')
+    main_library = cqm_measure.cql_libraries.select { |lib| lib.library_name == cqm_measure.main_cql_library }.first
+
+    # check the new library structure
+    expect(main_library).to_not be_nil
+    expect(main_library.library_name).to eq('MedianTimefromEDArrivaltoEDDepartureforDischargedEDPatients')
+    expect(main_library.library_version).to eq('7.2.002')
+    expect(main_library.statement_dependencies.size).to eq(13)
+    expect(main_library.elm_annotations).to eq(bonnie_measure.elm_annotations['MedianTimefromEDArrivaltoEDDepartureforDischargedEDPatients'])
+    expect(main_library.elm).to eq(bonnie_measure.elm[0])
+    expect(main_library.cql).to start_with('library MedianTimefromEDArrivaltoEDDepartureforDischargedEDPatients')
+
+    # check the references used by the "Initial Population"
+    ipp_dep = main_library.statement_dependencies.select { |dep| dep.statement_name == "Initial Population"}.first
+    expect(ipp_dep).to_not be_nil
+    expect(ipp_dep.statement_references.size).to eq(1)
+    expect(ipp_dep.statement_references.map(&:statement_name)).to include("ED Visit")
+
+    # Legacy fields that may be removed later
+    expect(cqm_measure.population_criteria).to eq(bonnie_measure.population_criteria)
+    expect(cqm_measure.data_criteria).to eq(bonnie_measure.data_criteria)
+    expect(cqm_measure.source_data_criteria).to eq(bonnie_measure.source_data_criteria)
+    expect(cqm_measure.measure_period).to eq(bonnie_measure.measure_period)
+    expect(cqm_measure.measure_attributes).to eq(bonnie_measure.measure_attributes)
+
+    # check population set
+    expect(cqm_measure.population_sets.size).to eq(1)
+    population_set = cqm_measure.population_sets[0]
+    expect(population_set.id).to eq('PopulationCriteria1')
+    expect(population_set.title).to eq('Population Criteria Section')
+    expect(population_set.populations).to be_instance_of(CQM::ContinuousVariablePopulationMap)
+    expect(population_set.populations.IPP.statement_name).to eq("Initial Population")
+  end
 end
